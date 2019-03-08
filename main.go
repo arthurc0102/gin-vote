2c4d09d876e921d4668dc096ef81c824eff82db3
Arthur Chang <arthurc0102@gmail.com>
2019-03-08 11:11:54 +0800

    Add cors

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"github.com/arthurc0102/gin-vote/config"
 	"github.com/arthurc0102/gin-vote/db"
 	"github.com/arthurc0102/gin-vote/db/migrate"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.Default())
 
 	config.Load()
 	config.Static(server)
